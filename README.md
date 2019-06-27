0927e1f7a343ad8eed5a1f365ca6f1cf12b7c16b
Vasily Smolin <vasily.smolin@spiti.ru>
2019-06-27 14:02:24 +0400

    f

---
+++ README.md
@@ -0,0 +1,19 @@
+# Helper Url 
+
+
+composer.json лоакльное подключение: 
+
+        {
+                "name": "splaa/testblog",
+                "minimum-stability": "dev",
+                "repositories": [
+                    {
+                        "type": "path",
+                        "url": "e:/sites/composer-packages/splx/helperurl"
+                    }
+                ],
+                "require": {
+                    "splx/helperurl": "1.0"
+                }
+            }
+
---
+++ src/splx/helperurl/HelperURL.php
@@ -0,0 +1,50 @@
+<?php
+
+    namespace vasilysmolin\helperurl;
+
+    class HelperURL
+    {
+        /**
+         * @return mixed
+         */
+
+         public function getCurPageURL()
+         {
+             $pageURL = 'http';
+             if(isset($_SERVER['HTTPS'])){
+                 if($_SERVER['HTTPS'] == 'on'){$pageURL .= "s";}
+             }
+             $pageURL .= '://';
+             if ($_SERVER['SERVER_PORT'] != '80') {
+                 $pageURL .= $_SERVER['SERVER_NAME'].':'
+	                 . $_SERVER['SERVER_PORT']
+                 . $_SERVER['SCRIPT_FILENAME'] 
+                 . $_SERVER['DOCUMENT_ROOT'];
+             }else{
+	             $pageURL .= $_SERVER["SERVER_NAME"] . $_SERVER['REQUEST_URI'] . ':' . $_SERVER['SERVER_PORT'];
+             }
+             return $pageURL;
+         }
+
+	    public function getCurPageURLHtmlString()
+	    {
+		    $pageURL = 'Server Name: ' . 'http';
+		    if (isset($_SERVER['HTTPS'])) {
+			    if ($_SERVER['HTTPS'] == 'on') {
+				    $pageURL .= "s";
+			    }
+		    }
+		    $pageURL .= '://';
+		    if ($_SERVER['SERVER_PORT'] != '80') {
+			    $pageURL .= $_SERVER['SERVER_NAME'] . ':'
+				    . $_SERVER['SERVER_PORT'] . '<br>'
+				    . 'File Name: ' . $_SERVER['SCRIPT_FILENAME'] . '<br>'
+				    . 'Document Root: ' . $_SERVER['DOCUMENT_ROOT'] . '<br>';
+		    } else {
+			    $pageURL .= $_SERVER["SERVER_NAME"] . $_SERVER['REQUEST_URI'] . ':' . $_SERVER['SERVER_PORT'];
+		    }
+		    return $pageURL;
+	    }
+
+    }
+?>
